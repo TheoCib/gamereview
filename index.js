44a314ebc0779597bd4982c4a2b853960f1ae654
TheoCib <theophile.cibert@ynov.com>
2018-05-17 16:30:48 +0200

    Authentification + profil + css

--- index.js
+++ index.js
@@ -1,16 +1,76 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Sequelize = require('sequelize');
-const app = express();
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
+//configuarations
+const COOKIE_SECRET = 'cookie secret';
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'pug');
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser(COOKIE_SECRET));
+app.use(session({
+    secret: COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Strategie d'authentification via email + password
+passport.use(new LocalStrategy((email, password, done) =>{
+    User
+        .findOne({where:{
+                email
+            }})
+        .then(function (user) {
+            if (!user || user.password !== password){
+                return done(null, false, {
+                    message: 'Email inconnue ou mdp invalide'
+                });
+            }
+            return done(null,user)
+        })
+        .catch(done);
+}));
+
+passport.serializeUser((user, cb) => {
+    cb(null, user.email);
+});
+passport.deserializeUser((email, cb) => {
+    User.findOne({where:{email}})
+        .then((user)=>{
+            cb(null,user)
+        })
+});
+
+//Que faire si les données sont justes/fausses
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}));
+
+//Se deconnecter
+app.get('/api/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+});
 
 //Initialisation des BDD
 const db = new Sequelize('gamereview', 'root', '', {
     host: 'localhost',
     dialect: 'mysql'
+});
+
+const User = db.define('user', {
+    firstname: { type: Sequelize.STRING },
+    lastname: { type: Sequelize.STRING },
+    email: { type: Sequelize.STRING },
+    password: { type: Sequelize.STRING }
 });
 
 const Post = db.define('post', {
@@ -53,19 +113,23 @@ Comment.belongsTo(Post);
 Post.sync();
 Vote.sync();
 Comment.sync();
+User.sync();
 
 //Routes
-app.get('/',(request, response) => {
+//Page principale
+app.get('/',(req, res) => {
     Post
         .findAll({include: [Vote]})
-        .then(posts => response.render("home", {posts}));
+        .then(posts => res.render("home", {posts, user: req.user}));
 
 });
 
+//Page ajouter post
 app.get('/addpost', (req,res) => {
-    res.render("addpost")
+    res.render("addpost", {user: req.user})
 });
 
+//Page detail
 app.get('/detail/:postId', (req,res) => {
     const postId = req.params.postId;
     Post
@@ -73,8 +137,18 @@ app.get('/detail/:postId', (req,res) => {
             id: postId
             }})
         .then((post) => {
-            res.render("detail", { post })
+            res.render("detail", { post, user: req.user })
         })
+});
+
+//page inscription
+app.get('/signup' ,(req,res) => {
+    res.render("signup")
+});
+
+//Page connexion, on passe
+app.get('/login' ,(req,res) => {
+    res.render("login", {user: req.user})
 });
 
 //Add une review dans la BDD
@@ -119,6 +193,24 @@ app.post('/detail/:postId', (req,res) =>{
             )
         })
         .then(() => res.redirect('/detail/' + req.params.postId))
+});
+
+//Inscription, mise en BDD d'un user
+app.post('/signup', (req,res) => {
+    User
+        .sync()
+        .then(() => {
+            User.create({
+                    firstname: req.body.firstname,
+                    lastname: req.body.lastname,
+                    email: req.body.email,
+                    password: req.body.password
+                }
+            )
+        })
+        .then(()=>{
+                    res.redirect('/')
+                })
 });
 
 
